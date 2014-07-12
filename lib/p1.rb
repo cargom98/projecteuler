a7764dfa89160404691f9514179d72a36fad7609
Carlos Gomez <cgomez@gomezmera.com>
2014-07-11 21:41:37 -0400

    Fixing rspec set up

---
+++ lib/p1.rb
@@ -0,0 +1,19 @@
+class Problema1
+
+	def initialize
+	end
+	
+  	def sum1(num,x)
+		nummod = num % x
+		topnumber = num - nummod
+		@total = (topnumber + x)*(topnumber * 0.5/x)
+		if nummod == 0 
+			@total = @total - num
+		end
+		return @total
+   	end
+
+  	def run(number)
+    	return sum1(number,3)+sum1(number,5)-sum1(number,15)
+	end
+end
---
+++ spec/p1_spec.rb
@@ -0,0 +1,7 @@
+# spec/your_class_spec.rb
+require 'spec_helper'
+require 'p1'
+
+describe Problema1  do
+	
+end
